8cd97185e99242e0c28a0b4ced13d3218ffdcc86
SatyaNihal <nihal.kodukula@icloud.com>
2025-01-25 15:40:06 -0500

    Update sr.py

--- sr.py
+++ sr.py
@@ -4,7 +4,7 @@ def speech_to_text():
     recognizer = sr.Recognizer()
 
     with sr.Microphone() as source:
-        print("Adjusting to fit background noise... Please wait.")
+        print("Please wait...")
         recognizer.adjust_for_ambient_noise(source, duration=1)
         print("Please go ahead... Listening...")
 
